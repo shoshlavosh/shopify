1884ade6ca69e197e7fc1a7b169b530636258287
shoshlavosh <shoshlavosh@gmail.com>
2021-09-25 23:47:20 +0000

    added one doc test to crud.py

--- crud.py
+++ crud.py
@@ -13,6 +13,19 @@ def create_user(email, password):
     return user
 
 
+def is_user1(name, email):
+    """Is this user1?
+    >>> is_user1("user1", "user0@test.com")
+    True
+    >>> is_user1("Jane", "jane@hacks.com")
+    False
+    >>> is_user1("user1", "banana@test.com")
+    True
+    >>> is_user1("Banana", "user0@test.com")
+    True
+    """
+    return name == "user1" or email == "user0@test.com"
+
 def get_users():
     """Return all users."""
 
